923be16323ae35303ed4949b48bfba9e1582ef9f
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-16 12:21:31 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,9 @@
  */
 int main(void)
 {
-	char s[60] = "\"Programming is like building a multilingual puzzle";	
+	char s[60] = "\"Programming is like building a multilingual puzzle";
 
 	puts(s);
 	return (0);
 }
+
