27873b0912590e3ab9a36542eabb0662b606e9dd
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-16 11:36:26 -0800

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char s[] = "Programming is like building a multilingual puzzle";
+	char s[60] = "\"Programming is like building a multilingual puzzle";	
 
 	puts(s);
 	return (0);
