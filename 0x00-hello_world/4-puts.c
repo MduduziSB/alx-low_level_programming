94c221d746bb6eb48f036d78a4b62d2d9d777179
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-16 08:46:43 -0800

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main-entry code
+ * This code uses puts to display data
+ * Return:always 0 since void takes no argument
+ */
+int main(void)
+{
+	char s[] = "Programming is like building a multilingual puzzle";
+
+	puts(s);
+	return (0);
+}
