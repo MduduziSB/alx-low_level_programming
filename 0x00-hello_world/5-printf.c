da3d115b3819792197cecf9f28c1cf73a99ad273
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-16 06:53:08 -0800

    Commented 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
-int main()
+/* Using printf to display data */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
