bdd54b38a5bb7c7a15d581fdf1708572ba0d60af
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-17 04:50:42 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main : Entry point
+ * working on unix to print a line on standard erro
+ * Return : Always 1 (Success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
