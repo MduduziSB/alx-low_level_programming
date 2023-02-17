45ae85ef250e21b191e3f2c8e20e4b9cc98d5933
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-17 13:34:34 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * this code displays last digit of a rando number
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	for (int alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
