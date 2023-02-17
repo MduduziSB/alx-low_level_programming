ccd213547c5953a3be9be580543dfeffad24304a
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-17 05:05:07 -0800

    101-quote new version

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include<unistd.h>
 
 /**
- * main : Entry point
+ * main - Entry point
  * working on unix to print a line on standard erro
- * Return : Always 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar", stdout);
+	fputs(", 2015-10-19\n", stdout);
 	return (1);
 }
