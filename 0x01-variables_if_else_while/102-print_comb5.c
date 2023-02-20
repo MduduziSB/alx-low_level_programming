374136dc1ab390b0ddff133b2ab4fe2ab8ba6070
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-20 11:51:32 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,57 +9,27 @@
  */
 int main(void)
 {
-	int d_00 = '0', d_01, d_10, d_11;
+	int fd = 0, ld;
 
-	while (d_00 <= '9')
+	while (fd <= 98)
 	{
-		d_01 = '0';
-		while (d_01 <= '8')
-		{
-			d_10 = '0';
-			while (d_10 <= '9')
+		ld = fd + 1;
+		do {
+			putchar((fd / 10) + '0');
+			putchar((fd % 10) + '0');
+			putchar(' ');
+			putchar((ld / 10) + '0');
+			putchar((ld % 10) + '0');
+			if (fd == 98 && ld == 99)
+				putchar('\n');
+			else
 			{
-				d_11 = '0';
-				do {
-					if (d_00 == '9' && d_01 >= '3')
-					{
-						d_10 = '9';
-						if (d_01 == '3' && d_11 < '5')
-							d_11 = '5';
-						if (d_01 == '4' && d_11 < '5')
-							d_11 = '5';
-						if (d_01 == '5' && d_11 < '6')
-							d_11 = '6';
-						if (d_01 == '6' && d_11 < '7')
-							d_11 = '7';
-						if (d_01 == '7' && d_11 < '8')
-							d_11 = '8';
-						if (d_01 == '8' && d_11 < '9')
-							d_11 = '9';
-					}
-					if (d_10 == '0' && d_11 == '0')
-						d_11++;
-					if (d_00 == d_10 && d_01 == d_11)
-						d_11++;
-					putchar(d_00);
-					putchar(d_01);
-					putchar(' ');
-					putchar(d_10);
-					putchar(d_11);
-					if ((d_00 == '9' && d_01 == '8') && (d_10 == '9' && d_11 == '9'))
-						putchar('\n');
-					else
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					d_11++;
-				} while (d_11 <= '9');
-				d_10++;
+				putchar(',');
+				putchar(' ');
 			}
-			d_01++;
-		}
-		d_00++;
+			ld++;
+		} while (ld <= 99);
+		fd++;
 	}
 	return (0);
 }
