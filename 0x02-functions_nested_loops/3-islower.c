3216186e098a89eb364af1edea9199d22c0e33d1
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-21 18:02:55 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * this code function detects whether character is lowercase or not
+ * Return: always 1 (Success)
+ */
+int _islower(int c)
+{
+	int i = 1, tmp = 'a', counter = 0;
+
+	do {
+		if (c == tmp)
+		{
+			counter++;
+			break;
+		}
+		tmp++;
+	} while (tmp <= 'z');
+	return (counter);
+}
