933f5fdc54e84ecfee12fec9352cc5bf9892042d
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-21 20:11:48 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * this code prints numbers in hexadecimal form
+ * @x: function argument
+ * Return: always 0 (Success)
+ */
+int print_last_digit(int x)
+{
+	if (x >= 0)
+	{
+		_putchar((x % 10) + '0');
+		return (x % 10);
+	}
+	else
+	{
+		x *= -1;
+		_putchar((x % 10) + '0');
+		return (x % 10);
+	}
+}
