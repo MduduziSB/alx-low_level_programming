92f1cad38b4275fe1cb9796e701a66cbdca0a7a5
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-21 20:44:04 -0800

    add task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * add - Entry point
+ * this code prints sum of two numbers
+ * @a: function argument
+ * @b: function argument
+ * Return: always 0 (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
