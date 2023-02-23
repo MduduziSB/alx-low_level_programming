0ab7cb95775342c89335183a945b14916ddec4d8
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-23 01:33:52 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * int _isupper - Entry point
+ * this function detects if a character is an uppercase
+ * @c: function argument
+ * Return: always 1 (Success) 0 (failure)
+ */
+int _isupper(int c)
+{
+	int tmp = 'A', count = 0;
+
+	do {
+		if (c == tmp)
+		{
+			count++;
+			break;
+		}
+		tmp++;
+	} while (tmp <= 'Z');
+	return (count);
+}
