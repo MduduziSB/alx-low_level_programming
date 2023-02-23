6e17dc65e40596fde248499853ae0a3017b5fe61
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-23 02:28:25 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * this function detects if a character is a digit from 0-9
+ * @c: function argument
+ * Return: always 1 (Success) 0 (failure)
+ */
+int _isdigit(int c)
+{
+	int tmp = '0', count = 0;
+
+	do {
+		if (c == tmp)
+		{
+			count++;
+			break;
+		}
+		tmp++;
+	} while (tmp <= '9');
+	return (count);
+}
+_putchar('\n');
