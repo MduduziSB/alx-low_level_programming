d524ad1f8b9fd81713444e56a971400ffa0b63d7
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-23 19:36:44 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,20 +3,6 @@
 #include "main.h"
 
 /**
- * spaces - prints spaces
- * @n: function parameter
- * Return: always 0
- */
-void spaces(int n)
-{
-	int k = 1;
-
-	do {
-		putchar(' ');
-		k++;
-	} while (k < n);
-}
-/**
  * print_diagonal - draws a diagonal line on the terminal
  * @n: function parameter
  * Return: always 0
@@ -25,22 +11,21 @@ void print_diagonal(int n)
 {
 	if (n <= 0)
 		_putchar('\n');
-	else if (n == 1)
+	else 
 	{
-		_putchar('\\');
-		_putchar('\n');
-	}
-	else
-	{
-		int j = 1;
+		int dia = 1, space;
 
 		do {
+			space = 1;
+			while (space < dia)
+			{
+				_putchar(' ');
+				space++;
+			}
 			_putchar('\\');
 			_putchar('\n');
-			j++;
-			if (j <= n)
-			spaces(j);
-		} while (j <= n);
+			dia++;
+		} while (dia <= n);
 	}
 }
 
