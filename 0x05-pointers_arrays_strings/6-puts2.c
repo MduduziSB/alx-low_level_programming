4f3051a2e0c7d751a51a7a5e7708f396877dfb2b
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-26 23:51:18 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other charecter
+ * @str: function parameter
+ * Return: always 0 (Success)
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	do {
+		if (i % 2 == 0)
+			_putchar(*(str + i));
+		i++;
+	} while (*(str + i) != '\0');
+	_putchar('\n');
+}
+
