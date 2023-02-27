1f77005f4ce82668819f226a8aa4c7034c81c82c
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-02-27 09:25:23 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,23 +9,24 @@
  */
 int _atoi(char *s)
 {
-	int num = 0, i = 0, count = 0;
+	unsigned int num = 0;
+	int i = 0, count = 0;
 
 	while (s[i] != '\0')
 	{
+		if (s[i] == '-')
+			count++;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			num = num * 10 + (s[i] - '0');
-			if (s[i - 1] == '-')
-				count++;
 			if (s[i + 1] > '9' || s[i + 1] < '0')
 				break;
 		}
 		i++;
 	}
-	if (count == 1)
-		return (-num);
-	else
+	if (count % 2 == 0)
 		return (num);
+	else
+		return (-num);
 }
 
