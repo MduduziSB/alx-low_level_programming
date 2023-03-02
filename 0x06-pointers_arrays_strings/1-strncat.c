0172dff573300203536e31adc73ff877f152b399
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-03-01 23:00:45 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,42 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
  * _strncat - concatenates two strings
  * @dest: first parameter
  * @src: second parameter
+ * @n: third parameter
  * Return: always 0 (Success)
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+	int i = 0, j = 0, size = 0, m;
 
 	while (dest[i] != '\0')
 		i++;
-	while (src[j] != '\0')
+	while (src[size] != '\0')
+		size++;
+	if (n > size)
+		m = size;
+	else
+		m = n;
+	while (m > 0 && src[j] != '\0')
 	{
-		dest[j] = src[j];
+		dest[i] = src[j];
 		j++;
+		i++;
+		m--;
 	}
-	dest[j] = src[j];
 	return ((char *)dest);
 }
-int main(void)
-{
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
 
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
-    printf("%s\n", s1);
-    printf("%s", s2);
-    printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 1024);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
-}
