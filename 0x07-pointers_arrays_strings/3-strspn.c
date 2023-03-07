2eaf0cb98ff45afbdd3ba6a7a15684aaee01d17a
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-03-06 17:59:42 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,47 +8,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0, j, count = 0, k = 0, ix;
+	unsigned int i = 0, j, count = 0, k;
 
 	while (s[i] != '\0')
 	{
-		j = 0;
 		k = 0;
+		j = 0;
+
 		while (accept[j] != '\0')
 		{
-			if (j > 0)
-			{
-				ix = j - 1;
-				while (ix >= 0)
-				{
-					if (accept[ix] == accept[j])
-					{
-						j++;
-						break;
-					}
-					ix--;
-				}
-			}
 			if (s[i] == accept[j])
 			{
 				count++;
 				k++;
 			}
+			if (accept[j + 1] == '\0' && k == 0)
+				return (count);
 			j++;
 		}
-		if (k == 0 && accept[j] == '\0')
-			return (count);
 		i++;
 	}
 	return (count);
 }
-int main(void)
-{
-    char *s = "hello, world";
-    char *f = "oleh";
-    unsigned int n;
 
-    n = _strspn(s, f);
-    printf("%u\n", n);
-    return (0);
-}
