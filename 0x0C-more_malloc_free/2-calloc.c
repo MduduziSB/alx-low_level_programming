2fc62ccccd819df5e0e258eb3c0dc76c3fb84851
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-03-16 13:32:24 -0700

    2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int  i = 0;
 
 	if (nmemb == 0 || size == 0)
-		return (NULL);
+		return (0);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
