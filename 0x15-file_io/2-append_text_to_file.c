3c31d77af4d6aa8b274f0f4dbcc4d81c9cee1582
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-04-14 07:29:53 -0700

    tasks 1 and 2, 0x15

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -30,10 +30,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	fp = open(filename, O_WRONLY | O_APPEND);
 	if (fp == -1)
 		return (-1);
-	strlen = _strlen(text_content);
-	fappend = write(fp, text_content, strlen);
-	if (fappend == -1)
-		return (-1);
+	if (text_content)
+	{
+		strlen = _strlen(text_content);
+		fappend = write(fp, text_content, strlen);
+		if (fappend == -1)
+			return (-1);
+	}
 	close(fp);
 	return (1);
 }
