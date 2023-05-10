5ef31a707dc9488db7aaf7ca33db9e72d89b789d
MduduziSB <sibusisom.skhumkane@gmail.c>
2023-05-10 08:06:27 -0700

    3-cp modified

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,23 +5,15 @@
 #include "main.h"
 /**
  * _error_check - checks for errors such as number arguments
- * @str: file names
- * @orig_file: integer
- * @cpy_file: integer
+ * @msg: error message
+ * @str: file name
+ * @ex_status: Exit status
  * Return: nothing
  */
-void _error_check(char **str, int orig_file, int cpy_file)
+void _error_check(const char *msg, char *str, size_t ex_status)
 {
-	if (orig_file == -1 || !str[1])
-	{
-		dprintf(STDERR_FILENO, "%s%s\n", "Error: Can't read from file ", str[1]);
-		exit(98);
-	}
-	if (cpy_file == -1 || str[2])
-	{
-		dprintf(STDERR_FILENO, "%s%s\n", "Error: Can't write to ", str[2]);
-		exit(99);
-	}
+	dprintf(STDERR_FILENO, "%s%s\n", msg, str);
+	exit(ex_status);
 }
 /**
  * main - copies the content of a file to another
@@ -31,6 +23,9 @@ void _error_check(char **str, int orig_file, int cpy_file)
  */
 int main(int argc, char **argv)
 {
+	const char *error_msg1 = "Error: Can't read from ";
+	const char *error_msg2 = "Error: Can't write to ";
+	size_t ex_status1 = 98, ex_status2 = 99;
 	int orig_file, cpy_file, write_to_cpy, read_cont, close_file;
 	char str[1024];
 
@@ -40,16 +35,19 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 	orig_file = open(argv[1], O_RDONLY);
+	read_cont = read(orig_file, str, 1024);
+	if (orig_file == -1 || read_cont == -1)
+		_error_check(error_msg1, argv[1], ex_status1);
 	cpy_file = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	_error_check(argv, orig_file, cpy_file);
-	while ((read_cont = read(orig_file, str, 1024)) > 0)
-	{
+
+	do {
 		write_to_cpy = write(cpy_file, str, read_cont);
-		if (write_to_cpy == -1)
-			_error_check(argv, read_cont, write_to_cpy);
-	}
-	if (read_cont == -1)
-		_error_check(argv, read_cont, cpy_file);
+		if (write_to_cpy == -1 || cpy_file == -1)
+			_error_check(error_msg2, argv[2], ex_status2);
+		read_cont = read(orig_file, str, 1024);
+		if (orig_file == -1 || read_cont == -1)
+			_error_check(error_msg1, argv[1], ex_status1);
+	} while (read_cont > 0);
 	close_file = close(orig_file);
 	if (close_file == -1)
 	{
