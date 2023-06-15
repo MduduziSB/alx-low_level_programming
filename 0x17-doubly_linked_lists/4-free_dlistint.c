eab4de42696d9b5b90c5c30f552df6d6d7845a2e
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-06-15 10:42:01 -0700

    free_dlistint

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: linked dlistint_t list head
+ * Return: Nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr = head;
+	while (head)
+	{
+		head = head->next;
+		free(ptr);
+		ptr = head;
+	}
+}
+
