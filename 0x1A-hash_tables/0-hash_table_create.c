26d7e41a90cd2fb30fa58447d442b344c320abc8
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-07-06 16:35:26 -0700

    0-hash_table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,6 +7,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
 	table->size = size;
@@ -15,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!table || !table->array)
 		return (NULL);
 
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 	return (table);
 }
