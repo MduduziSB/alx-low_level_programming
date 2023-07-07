23b45c523ae3e58a49e68e1c8fffa2a89ccdeed6
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-07-07 15:56:36 -0700

    corrected task4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *ptr;
 
+	if (!ht || !key || !key[0])
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 	ptr = ht->array[index];
 
 	while (ptr)
