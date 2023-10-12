3e30a66846bd841650f34bc5e5ded8d4960133c7
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-10-11 20:59:58 -0700

    Some Advanced Tsaks

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+/**
+ * jump_search - searches for a value in an array using Jump search algorithm
+ * @array: array of integers
+ * @size: array size
+ * @value: value to search for
+ * Return: Element index if found, -1 otherwise
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t j = 0, i = 0, step = sqrt(size);
+
+	if (!array)
+		return (-1);
+
+	while (i < size && array[i] <= value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		j = i;
+		i += step;
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n", j, i);
+	i = i < size - 1 ? i : size - 1;
+
+	while (j < i && array[j] < value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", j, array[j]);
+		if (array[j] == value)
+			return (j);
+		j++;
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,10 +1,30 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	int index;
+	struct listint_s *next;
+} listint_t;
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <math.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t i, size_t size);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 #endif
 
